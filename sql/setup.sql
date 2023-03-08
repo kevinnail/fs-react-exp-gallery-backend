c885837aeeae32f1c65ce3b589fe25d60d0d4630
kevinnail <fumalicious@gmail.com>
2023-03-07 23:27:44 -0800

    db set up, table name change

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,8 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users_admin;
 
-CREATE TABLE users (
+CREATE TABLE users_admin (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   email VARCHAR,
   password_hash VARCHAR NOT NULL,
