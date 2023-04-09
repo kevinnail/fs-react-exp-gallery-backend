844fcb3972cb6e89236ebd76ff875d85a1f15933
kevinnail <fumalicious@gmail.com>
2023-04-08 18:57:32 -0700

    updated tests removed snapshots

--- __tests__/posts.test.js
+++ __tests__/posts.test.js
@@ -295,89 +295,71 @@ describe('admin gallery routes', () => {
   it('GET /api/v1/main-gallery should return all posts', async () => {
     const data = await request(app).get('/api/v1/main-gallery');
     expect(data.status).toBe(200);
-    expect(data.body).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "author_id": "1",
-          "category": "Test 1",
-          "created_at": "2023-04-04T01:05:38.642Z",
-          "description": "Test 1",
-          "id": "1",
-          "image_url": "Test 1",
-          "num_imgs": "1",
-          "price": "Test 1",
-          "public_id": "Test 1",
-          "title": "Test 1",
-        },
-        Object {
-          "author_id": "1",
-          "category": "Test 2",
-          "created_at": "2023-04-04T01:05:38.642Z",
-          "description": "Test 2",
-          "id": "2",
-          "image_url": "Test 2",
-          "num_imgs": "1",
-          "price": "Test 2",
-          "public_id": "Test 2",
-          "title": "Test 2",
-        },
-        Object {
-          "author_id": "1",
-          "category": "Test 3",
-          "created_at": "2023-04-04T01:05:38.642Z",
-          "description": "Test 3",
-          "id": "3",
-          "image_url": "Test 3",
-          "num_imgs": "1",
-          "price": "Test 3",
-          "public_id": "Test 3",
-          "title": "Test 3",
-        },
-      ]
-    `);
+    expect(data.body).toEqual([
+      {
+        author_id: '1',
+        category: 'Test 1',
+        created_at: expect.any(String),
+        description: 'Test 1',
+        id: '1',
+        image_url: 'Test 1',
+        num_imgs: '1',
+        price: 'Test 1',
+        public_id: 'Test 1',
+        title: 'Test 1',
+      },
+      {
+        author_id: '1',
+        category: 'Test 2',
+        created_at: expect.any(String),
+        description: 'Test 2',
+        id: '2',
+        image_url: 'Test 2',
+        num_imgs: '1',
+        price: 'Test 2',
+        public_id: 'Test 2',
+        title: 'Test 2',
+      },
+      {
+        author_id: '1',
+        category: 'Test 3',
+        created_at: expect.any(String),
+        description: 'Test 3',
+        id: '3',
+        image_url: 'Test 3',
+        num_imgs: '1',
+        price: 'Test 3',
+        public_id: 'Test 3',
+        title: 'Test 3',
+      },
+    ]);
   });
 
   it('GET /api/v1/main-gallery/:id should return a single post', async () => {
     const data = await request(app).get('/api/v1/main-gallery/1');
     expect(data.status).toBe(200);
-    expect(data.body).toMatchInlineSnapshot(`
-      Object {
-        "author_id": "1",
-        "category": "Test 1",
-        "created_at": "2023-04-04T01:05:38.735Z",
-        "description": "Test 1",
-        "id": "1",
-        "image_url": "Test 1",
-        "num_imgs": "1",
-        "price": "Test 1",
-        "public_id": "Test 1",
-        "title": "Test 1",
-      }
-    `);
+    expect(data.body).toEqual({
+      author_id: '1',
+      category: 'Test 1',
+      created_at: expect.any(String),
+      description: 'Test 1',
+      id: '1',
+      image_url: 'Test 1',
+      num_imgs: '1',
+      price: 'Test 1',
+      public_id: 'Test 1',
+      title: 'Test 1',
+    });
   });
 
   it('GET /api/v1/main-gallery/urls/:id should return all urls for a post', async () => {
     const data = await request(app).get('/api/v1/main-gallery/urls/1');
     expect(data.status).toBe(200);
-    expect(data.body).toMatchInlineSnapshot(`
-      Array [
-        Object {
-          "id": 1,
-          "image_url": "image_url.com",
-          "public_id": "public_id_1",
-        },
-        Object {
-          "id": 2,
-          "image_url": "image_url.com2",
-          "public_id": "public_id_2",
-        },
-        Object {
-          "id": 3,
-          "image_url": "image_url.com3",
-          "public_id": "public_id_3",
-        },
-      ]
-    `);
+    expect(data.body).toEqual([
+      { id: 1, image_url: 'image_url.com', public_id: 'public_id_1' },
+      { id: 2, image_url: 'image_url.com2', public_id: 'public_id_2' },
+      { id: 3, image_url: 'image_url.com3', public_id: 'public_id_3' },
+    ]);
   });
 
   // don't remove this one: '});'   VVVVV ---- TEST ABOVE///////////////////////////////////////////////////////
