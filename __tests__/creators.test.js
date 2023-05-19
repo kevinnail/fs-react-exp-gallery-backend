b20f4d2f07cdaca6d60bd96d72742fc3a11fab18
Tyler Watson <tylerww91@gmail.com>
2023-05-19 15:00:40 -0700

    builds creators test file, adds initial test

---
+++ __tests__/creators.test.js
@@ -0,0 +1,42 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
+
+// Dummy user for testing
+const mockUser = {
+  email: 'test@example.com',
+  password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
+
+  // Create a user to sign in with
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  // ...then sign in
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
+describe('creators routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('GET /api/v1/creators should return a list of creators', async () => {
+    const resp = await request(app).get('/api/v1/creators');
+    expect(resp.status).toEqual(200);
+
+  })
+});
