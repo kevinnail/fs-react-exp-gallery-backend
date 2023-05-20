524ff990c7626a85aa0575bf3373d2b6d0173664
Tyler Watson <tylerww91@gmail.com>
2023-05-20 12:42:00 -0700

    builds GET /api/v1/creators/:id route test / POST

--- __tests__/creators.test.js
+++ __tests__/creators.test.js
@@ -2,29 +2,29 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const UserService = require('../lib/services/UserService');
+// const UserService = require('../lib/services/UserService');
 
 // Dummy user for testing
-const mockUser = {
-  email: 'test@example.com',
-  password: '12345',
-};
+// const mockUser = {
+//   email: 'test@example.com',
+//   password: '12345',
+// };
 
-const registerAndLogin = async (userProps = {}) => {
-  const password = userProps.password ?? mockUser.password;
+// const registerAndLogin = async (userProps = {}) => {
+//   const password = userProps.password ?? mockUser.password;
 
-  // Create an "agent" that gives us the ability
-  // to store cookies between requests in a test
-  const agent = request.agent(app);
+//   // Create an "agent" that gives us the ability
+//   // to store cookies between requests in a test
+//   const agent = request.agent(app);
 
-  // Create a user to sign in with
-  const user = await UserService.create({ ...mockUser, ...userProps });
+//   // Create a user to sign in with
+//   const user = await UserService.create({ ...mockUser, ...userProps });
 
-  // ...then sign in
-  const { email } = user;
-  await agent.post('/api/v1/users/sessions').send({ email, password });
-  return [agent, user];
-};
+//   // ...then sign in
+//   const { email } = user;
+//   await agent.post('/api/v1/users/sessions').send({ email, password });
+//   return [agent, user];
+// };
 
 describe('creators routes', () => {
   beforeEach(() => {
@@ -41,6 +41,7 @@ describe('creators routes', () => {
       Array [
         Object {
           "artist_name": "Brutus",
+          "bio": "I blew glass for Julius Caesar himself",
           "email": "Brutus@gmail.com",
           "first_name": "Jeff",
           "id": "1",
@@ -48,6 +49,7 @@ describe('creators routes', () => {
         },
         Object {
           "artist_name": "Highly Educated",
+          "bio": "PHD in blowing glass",
           "email": "Highlyeducated@gmail.com",
           "first_name": "Bob",
           "id": "2",
@@ -55,5 +57,40 @@ describe('creators routes', () => {
         },
       ]
     `);
+  });
+
+  it('GET /api/v1/creators/:id should return an individual creator', async () => {
+    const resp = await request(app).get('/api/v1/creators/1');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+      Object {
+        "artist_name": "Brutus",
+        "bio": "I blew glass for Julius Caesar himself",
+        "email": "Brutus@gmail.com",
+        "first_name": "Jeff",
+        "id": "1",
+        "last_name": "Stevens",
+      }
+    `);
+  });
+
+  it('POST /api/v1/creators should add a new creator', async () => {
+    const newCreator = {
+      artist_name: 'Mobius',
+      first_name: 'John',
+      last_name: 'M',
+      email: 'mobius@gmail.com',
+      bio: 'Maximum airflow',
+    };
+    const resp = await request(app).post('/api/v1/creators').send(newCreator);
+    expect(resp.status).toBe(200);
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      artist_name: newCreator.artist_name,
+      first_name: newCreator.first_name,
+      last_name: newCreator.last_name,
+      email: newCreator.email,
+      bio: newCreator.bio,
+    });
   });
 });
