529ac1aee1310c98c8c1a4caf8b7e2a0a25c1c8c
kevinnail <fumalicious@gmail.com>
2025-03-10 17:53:29 -0700

    update snapshot

--- __tests__/download.test.js
+++ __tests__/download.test.js
@@ -53,9 +53,9 @@ describe('csv file download route', () => {
     expect(csvResp.headers['content-type']).toBe('text/csv; charset=utf-8');
     expect(csvResp.text).toMatchInlineSnapshot(`
       "\\"created_at\\",\\"title\\",\\"description\\",\\"image_url\\",\\"category\\",\\"price\\"
-      \\"03/03/2025\\",\\"Test 1\\",\\"Test 1\\",\\"Test 1\\",\\"Test 1\\",\\"Test 1\\"
-      \\"03/03/2025\\",\\"Test 2\\",\\"Test 2\\",\\"Test 2\\",\\"Test 2\\",\\"Test 2\\"
-      \\"03/03/2025\\",\\"Test 3\\",\\"Test 3\\",\\"Test 3\\",\\"Test 3\\",\\"Test 3\\""
+      \\"03/10/2025\\",\\"Test 1\\",\\"Test 1\\",\\"Test 1\\",\\"Test 1\\",\\"Test 1\\"
+      \\"03/10/2025\\",\\"Test 2\\",\\"Test 2\\",\\"Test 2\\",\\"Test 2\\",\\"Test 2\\"
+      \\"03/10/2025\\",\\"Test 3\\",\\"Test 3\\",\\"Test 3\\",\\"Test 3\\",\\"Test 3\\""
     `);
   });
 });
