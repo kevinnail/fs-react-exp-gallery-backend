a6ff639d70602bb96095d506764944d7a0e99b43
kevinnail <fumalicious@gmail.com>
2025-10-02 23:30:15 -0700

    update snapshot for test

--- __tests__/download.test.js
+++ __tests__/download.test.js
@@ -53,9 +53,9 @@ describe('csv file download route', () => {
     expect(csvResp.headers['content-type']).toBe('text/csv; charset=utf-8');
     expect(csvResp.text).toMatchInlineSnapshot(`
       ""created_at","title","description","image_url","category","price"
-      "09/29/2025","Test 1","Test 1","Test 1","Test 1","Test 1"
-      "09/29/2025","Test 2","Test 2","Test 2","Test 2","Test 2"
-      "09/29/2025","Test 3","Test 3","Test 3","Test 3","Test 3""
+      "10/02/2025","Test 1","Test 1","Test 1","Test 1","Test 1"
+      "10/02/2025","Test 2","Test 2","Test 2","Test 2","Test 2"
+      "10/02/2025","Test 3","Test 3","Test 3","Test 3","Test 3""
     `);
   });
 });
