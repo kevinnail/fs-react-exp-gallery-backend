b595d960ec3c7ef0f74fdc7ff86dc784dee93b27
kevinnail <fumalicious@gmail.com>
2025-10-04 21:45:35 -0700

    swap emoticon

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const API_URL = process.env.API_URL || 'http://localhost';
 const PORT = process.env.PORT || 7890;
 
 app.listen(PORT, () => {
-  console.info(`🚀  Server started on ${API_URL}:${PORT}`);
+  console.info(`✅  Server started on ${API_URL}:${PORT}`);
 });
 
 process.on('exit', () => {
