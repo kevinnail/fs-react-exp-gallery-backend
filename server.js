12ccef22648e9f2f12cdfedbdf27bd4b98d251c4
kevinnail <fumalicious@gmail.com>
2025-10-15 11:07:42 -0700

    add initialization of auction notifications to server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const app = require('./lib/app');
 const pool = require('./lib/utils/pool');
 const WebSocketService = require('./lib/services/websocket');
 const { initAuctionTimers } = require('./lib/jobs/auctionTimers.js');
+const { initNotificationCleanup } = require('./lib/jobs/notificationsCleanup.js');
 
 const API_URL = process.env.API_URL || 'http://localhost';
 const PORT = process.env.PORT || 7890;
@@ -9,6 +10,7 @@ const PORT = process.env.PORT || 7890;
 const server = app.listen(PORT, async () => {
   console.info(`✅  Server started on ${API_URL}:${PORT}`);
   await initAuctionTimers();
+  await initNotificationCleanup();
 });
 
 // Initialize WebSocket service
